b40e11dd8d2a12e8521a0d87d1bf90624a038a95
David Casanellas <davidcasanell@gmail.com>
2017-08-09 16:59:03 +0200

    chore: sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,28 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { StoreModule } from '@ngrx/store';
-import { heroesReducer } from './reducers/heroes';
+import { MdSidenavModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { heroesReducer } from './reducers/heroes';
 import { HeroListComponent } from './hero-list/hero-list.component';
 import { HeroService } from './hero.service';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroListComponent
+    HeroListComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    StoreModule.forRoot({ heroes: heroesReducer })
+    StoreModule.forRoot({ heroes: heroesReducer }),
+    BrowserAnimationsModule,
+    MdSidenavModule
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
